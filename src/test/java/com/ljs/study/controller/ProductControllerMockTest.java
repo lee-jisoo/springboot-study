b6597a66c98e02783f81b4a40fbb8c4bdcb13e7d
이지수 <leejisoo@webtoonscorp.com>
2022-05-10 23:23:26 +0900

    WebMvcTest개발

--- src/test/java/com/ljs/study/controller/ProductControllerMockTest.java
+++ src/test/java/com/ljs/study/controller/ProductControllerMockTest.java
@@ -3,7 +3,6 @@ package com.ljs.study.controller;
 import com.ljs.study.service.ProductService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,15 +11,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+// Controller 만 MockTest
 @WebMvcTest(ProductController.class)
-public class ProductControllerTest {
+public class ProductControllerMockTest {
 
     @Autowired
     private MockMvc mvc;
@@ -28,6 +27,7 @@ public class ProductControllerTest {
     @MockBean
     private ProductService productService;
 
+    @DisplayName("[GET] /product/all")
     @Test
     public void getProduct() throws Exception {
         final List<String> list = List.of("갤럭시");
@@ -40,12 +40,21 @@ public class ProductControllerTest {
                 .andDo(print());
     }
 
+    @DisplayName("[POST] /product/insert")
     @Test
     public void insertProduct() throws Exception {
         mvc.perform(post(("/product/insert"))
                 .param("productName", "아이패드"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("OK"))
+                .andDo(print());
+    }
+
+    @DisplayName("[POST] /product/insert (파라메터 없는 경우 에러케이스)")
+    @Test
+    public void insertProduct2() throws Exception {
+        mvc.perform(post(("/product/insert")))
+                .andExpect(status().isBadRequest()) // 400 에러 검증
                 .andDo(print());
     }
 
